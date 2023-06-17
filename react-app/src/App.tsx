858f3b44e7637e9b16d7975a42c5a84000cb53d7
Reese Hatfield <hatfield.69@wright.edu>
2023-06-17 15:03:56 -0400

    Called onClick as a child

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,26 +2,27 @@ import ListGroup from "./components/ListGroup/ListGroup";
 import Button from "./components/Button/Button";
 import PasswordField from "./components/PasswordField/PasswordField";
 
-function App(){
+function App() {
+  const items: string[] = ["Paris", "Tokyo", "Chillicothe", "Fairborn"];
 
-    const items : string[] =[
-        'Paris',
-        'Tokyo',
-        'Chillicothe',
-        'Fairborn'
-    ]
+  const header: string = "Cities";
 
-    const header: string = 'Cities'
+  const handleSelectItemEvent = (item: string) => {
+    alert(item);
+  };
 
-    return<div>
-        <ListGroup
-            header={header}
-            listItems={items}
-        ></ListGroup>
+  return (
+    <div>
+      <ListGroup
+        header={header}
+        listItems={items}
+        onSelectItem={handleSelectItemEvent}
+      ></ListGroup>
 
-        <Button></Button>
-        <PasswordField></PasswordField>
-    </div>;
+      <Button></Button>
+      <PasswordField></PasswordField>
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -3,85 +3,77 @@ JSX = JavaScript XML
 -> define what html to return
  */
 
-
 /*
-whe you wanna return multiple top level
+when you want to return multiple top level
 html elements, wrap it in a div
 or better, use a Fragment
 even better, instead of
 <Fragment></Fragment>, use <> </>
  */
 
-
 /*
 Cannot iterate with JSX
  */
 
-
 //when rendering a list, each child in list should have a unique key
-import {ReactComponentElement} from "react";
+import { ReactComponentElement } from "react";
 import { MouseEvent } from "react";
 
-import { useState } from "react"
+import { useState } from "react";
 
-
-interface ListGroupProps{
-    header: string
-    listItems: any
+interface ListGroupProps {
+  header: string;
+  listItems: any;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup(props: ListGroupProps){
+function ListGroup({ header, listItems, onSelectItem }: ListGroupProps) {
+  //can also destructure props like function ListGroup({ header, listItems }: ListGroupProps)
+  //to just use header and listItems directly
 
-    //can also destructure props like function ListGroup({ header, listItems }: ListGroupProps)
-    //to just use header and listItems directly
+  const [selectedListItem, setSelectedListItem] = useState(-1);
 
-
-
-    const [selectedListItem, setSelectedListItem] = useState(-1)
-
-
-    if(props.listItems.length === 0){
-        return (
-            <>
-                {props.header}
-                <p>No cities found</p>
-            </>
-        )
-    }
-
-
-
-    const mapped_items = props.listItems.map((item: string, index: number) => {
-        return (
-            <li
-                key={index}
-                onClick={() => {
-                    // Use the state updating function instead of directly setting the variable
-                    setSelectedListItem(index);
-                }}
-                className={determineClasses(selectedListItem, index)}
-            >
-                {item}
-            </li>
-        );
-    });
-
-    return(
-        <>
-            {props.header}
-            <ul className="list-group"> {mapped_items}</ul>
-        </>
+  if (listItems.length === 0) {
+    return (
+      <>
+        {header}
+        <p>No cities found</p>
+      </>
     );
+  }
+
+  const mapped_items = listItems.map((item: string, index: number) => {
+    return (
+      <li
+        key={index}
+        onClick={() => {
+          // Use the state updating function instead of directly setting the variable
+          setSelectedListItem(index);
+          onSelectItem(item);
+        }}
+        className={determineClasses(selectedListItem, index)}
+      >
+        {item}
+      </li>
+    );
+  });
+
+  return (
+    <>
+      {header}
+      <ul className="list-group"> {mapped_items}</ul>
+    </>
+  );
 }
 
-function determineClasses(selected: number, index: number): string{
-    let classes: string = 'list-group-item'
+function determineClasses(selected: number, index: number): string {
+  let classes: string = "list-group-item";
 
-    if(selected === index){
-        classes += ' active'
-    }
+  if (selected === index) {
+    classes += " active";
+  }
 
-    return classes
+  return classes;
 }
 
-export default ListGroup
+export default ListGroup;
