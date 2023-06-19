b1351315065b599b1ba25c29f4afa5a69690c040
Ean Hatfield <hatfield.69@wright.edu>
2023-06-19 09:53:56 -0400

    Changed directory structure again

--- app/electron.ts
+++ app/electron.ts
@@ -1,29 +1,29 @@
-const { app, BrowserWindow } = require('app/electron')
-const isDev = require('electron-is-dev')
-const path = require('path')
-const url = require('url')
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const url = require("url");
 
-function createWindow () {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    })
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    if (isDev) {
-        win.loadURL('http://127.0.0.1:5173')
-    } else {
-        win.loadURL(
-            url.format({
-                pathname: path.join(__dirname, '../dist/index.html'),
-                protocol: 'file:',
-                slashes: true
-            })
-        )
-    }
+  if (isDev) {
+    win.loadURL("http://127.0.0.1:5173");
+  } else {
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "../dist/index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    );
+  }
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
