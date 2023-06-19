b40b8bb9a7909fa41c4a2988853ee55815b2dd6c
Ean Hatfield <hatfield.69@wright.edu>
2023-06-19 09:26:44 -0400

    Moved files to parent dir

--- app/electron.ts
+++ app/electron.ts
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('app/electron')
 const isDev = require('electron-is-dev')
 const path = require('path')
 const url = require('url')
