bba97be4f0154e7639e79c93a03133f9d56e5874
Reese Hatfield <hatfield.69@wright.edu>
2023-06-20 10:00:37 -0400

    Finalize pw hashing

--- app/backend/hashing/hash.ts
+++ app/backend/hashing/hash.ts
@@ -3,7 +3,6 @@
 export default function hashValue(toHash: string, publicKey: string){
     const hexPassword: string = encodeStringToHex(toHash);
     const privateKey: string = hexPassword;
-    console.log(`Private Key: ${privateKey}, Public Key ${publicKey}`);
 
     const mergedKeys: string = mergeKeys(publicKey, privateKey)
 
@@ -29,12 +28,66 @@ function encodeStringToHex(s: string): string {
 function mergeKeys(publicKey: string, privateKey: string): string{
     //todo
 
-    return '';
+    const isEven = (n: number) => n % 2 == 0
+
+    let chunks: string[] = []
+    let currentChunk: string = '';
+
+    for(let i = 0; i < privateKey.length; i ++){
+
+        const currentDigit: number = parseInt(privateKey.charAt(i), 16);
+
+        currentChunk += currentDigit;
+
+        if(isEven(currentDigit)){
+            chunks.push(currentChunk);
+            currentChunk = '';
+        }
+
+    }
+
+    const mergedKeys: string = distributeStrings(publicKey, chunks)
+
+    const finalHash: string = circularShiftNBits(mergedKeys, parseInt(mergedKeys.charAt(0)))
+
+    console.log(finalHash)
+
+    return finalHash
+
 }
 
-function circularShiftNBits(hashToShift: string, numBitsToShift): string{
-    //todo
-    return '';
+function distributeStrings(publicKey: string, chunks: string[]) {
+    if (!chunks.length) return publicKey;
+
+    let interval: number = Math.floor(publicKey.length / (chunks.length + 1));
+    let result: string = '';
+    let arrIndex: number = 0;
+
+    for(let i: number = 0; i <= publicKey.length; i += interval){
+        // Add the string portion
+        result += publicKey.substring(i, i + interval);
+
+        // assuming there are still array elements to distribute, add one
+        // to make distribution evn
+        if(arrIndex < chunks.length){
+            result += chunks[arrIndex];
+            arrIndex++;
+        }
+    }
+
+
+    if (publicKey.length % interval !== 0) {
+        result += publicKey.substring(interval * (chunks.length + 1));
+    }
+
+    return result;
+}
+
+function circularShiftNBits(hashToShift: string, numBitsToShift: number): string{
+    let len: number = hashToShift.length;
+
+    numBitsToShift = numBitsToShift % len;
+    return hashToShift.substring(numBitsToShift) + hashToShift.substring(0, numBitsToShift);
 }
 
 
