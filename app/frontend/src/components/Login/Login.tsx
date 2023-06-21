93cdc7afb0cf785e1c339a5872bdb9e9ba09aa6e
Reese Hatfield <hatfield.69@wright.edu>
2023-06-21 19:59:46 -0400

    Temp fix for login

--- app/frontend/src/components/Login/Login.tsx
+++ app/frontend/src/components/Login/Login.tsx
@@ -1,19 +1,53 @@
 import './Login.css'
 import * as React from "react";
-import { DataBase } from "../../../../backend/database/DataBase";
+import { useState } from "react";
 
-function Login(){
+interface LoginProps {}
 
-    //let db: DataBase = new DataBase;
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.currentTarget.value);
+    }
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const handleBtnClick = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        console.log("handling click event");
+        if(username === 'Admin' && password === 'Admin'){
+            alert("Logged in");
+        }
+    }
 
     return (
         <div className="login-container">
-            <label htmlFor="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required />
-            <label htmlFor="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required />
+            <form onSubmit={handleBtnClick}>
+                <label htmlFor="uname"><b>Username</b></label>
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    name="uname"
+                    required
+                    onChange={handleUsernameChange}
+                />
 
-            <button type="submit" className="loginBtn">Login</button>
+                <label htmlFor="psw"><b>Password</b></label>
+                <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="psw"
+                    onChange={handlePasswordChange}
+                    required
+                />
+
+                <button type="submit" className="loginBtn">Login</button>
+            </form>
         </div>
     )
 }
