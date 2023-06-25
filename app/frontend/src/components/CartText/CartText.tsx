a50c379ef68916f2d715cc217b14adec0f0c2216
Ean Hatfield <hatfield.69@wright.edu>
2023-06-25 18:57:43 -0400

    Added array typing

--- app/frontend/src/components/CartText/CartText.tsx
+++ app/frontend/src/components/CartText/CartText.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./CartText.css";
 
 interface CartTextProps {
-  cart: { name: string; price: number };
+  cart: any[];
 }
 
 function CartText({ cart }: CartTextProps) {
